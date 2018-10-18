c604aa2c8f8d1d0b4965bc7ded67e890677131fd
seki93 <ssekeres21@gmail.com>
2018-10-18 12:31:14 +0200

    Napravljen glavna klasa i model

--- src/main/java/model/News.java
+++ src/main/java/model/News.java
@@ -1,5 +1,91 @@
 package model;
 
-@
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "news")
 public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String title;
+
+    private String description;
+
+    private String text;
+
+    private Date date;
+
+    private boolean active;
+
+    private String categorty;
+
+    private String city;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getCategorty() {
+        return categorty;
+    }
+
+    public void setCategorty(String categorty) {
+        this.categorty = categorty;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
