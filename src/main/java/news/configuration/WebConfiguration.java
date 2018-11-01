0d8765d18a3a3e5cfdf6450dc1a3dcf4bc767787
seki93 <ssekeres21@gmail.com>
2018-11-01 10:08:34 +0100

    Cors

--- src/main/java/news/configuration/WebConfiguration.java
+++ src/main/java/news/configuration/WebConfiguration.java
@@ -49,7 +49,9 @@ public class WebConfiguration implements WebMvcConfigurer {
     }
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) { registry.addMapping("/**"); }
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
