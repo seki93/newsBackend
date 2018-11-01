9dd1cd031471b930e9c172f68c31d0bd1f27366e
RajkoJegdic <rajkojegdic@yahoo.com>
2018-11-01 09:49:07 +0100

    added cors origin

--- src/main/java/news/configuration/SecurityConfiguration.java
+++ src/main/java/news/configuration/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package news.security;
+package news.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
